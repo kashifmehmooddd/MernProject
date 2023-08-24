ea4cb9741fef918d872e44e5944fadc825803cd6
Kashif Mehmood <mehmoodkashif3054@gmail.com>
2023-08-24 11:43:45 +0500

    frontend setup

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const authRouter = require('./routes/api/auth');
 
 const app = express()
 
-const PORT = process.env.port || 3000
+const PORT = process.env.port || 3001
 
 connectDb();
 app.use(express.json({ extended: false }))
